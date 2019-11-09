1ce6c055142fc9ee64dba71e9ec94d604f6faec7
limaoxuan <563157030@qq.com>
2019-11-08 18:43:27 -0600

    gateway add api-key

--- services/gateway/src/main/java/com/max/gateway/filter/PreFilter.java
+++ services/gateway/src/main/java/com/max/gateway/filter/PreFilter.java
@@ -3,10 +3,17 @@ package com.max.gateway.filter;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.servlet.http.HttpServletRequest;
 
 public class PreFilter extends ZuulFilter {
+    @Value("${router.auth.url}")
+    private String authUrl;
+
+    @Value("${api.key}")
+    private String apiKey;
+
     @Override
     public String filterType() {
         return "pre";
@@ -27,7 +34,14 @@ public class PreFilter extends ZuulFilter {
 
         RequestContext ctx = RequestContext.getCurrentContext();
         HttpServletRequest request = ctx.getRequest();
+        // add zuul api-key header for  authorization
+        ctx.addZuulRequestHeader("api-key",apiKey);
+
         System.out.println(request.getRequestURL());
+        System.out.println(apiKey);
+
+
+
 
 
         return null;
